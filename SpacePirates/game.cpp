eeb7ee02dcd7683341cea5e7690169667c06a4d1
Rob Prouse <rob@prouse.org>
2019-06-16 16:54:49 -0400

    Starting ship movement

--- SpacePirates/game.cpp
+++ SpacePirates/game.cpp
@@ -1,10 +1,31 @@
 #include "game.h"
 
-void gamePlay()
+int playerX = 10;
+int playerY = 28;
+
+void input()
 {
-    arduboy.setCursor(0, 10);
-    arduboy.println("The game is on");
-    arduboy.println("Press A to END");
+    if(arduboy.pressed(UP_BUTTON))
+    {
+        if(playerY > 0)
+            --playerY;
+    }
+    else if(arduboy.pressed(DOWN_BUTTON))
+    {
+        if(playerY < 56)
+            ++playerY;
+    }
     if(arduboy.justPressed(A_BUTTON))
         gameState = STATE_GAME_OVER;
+}
+
+void draw()
+{
+    arduboy.drawRect(playerX, playerY, 16, 8);
+}
+
+void gamePlay()
+{
+    input();
+    draw();
 }
